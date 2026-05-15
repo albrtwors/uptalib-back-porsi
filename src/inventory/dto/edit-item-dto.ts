094975b4b99219d237c8afb53cb5b827d05ab570
albrtwors <albertojgsoto@gmail.com>
2026-05-14 21:19:47 -0400

    asdf

--- src/inventory/dto/edit-item-dto.ts
+++ src/inventory/dto/edit-item-dto.ts
@@ -1,6 +1,7 @@
 import { PartialType } from "@nestjs/swagger";
 import { CreateItemInventory } from "./create-item-dto";
+import { CreateInventoryDto } from "./create-inventory.dto";
 
-export class EditItemInventory extends PartialType(CreateItemInventory) {
+export class EditItemInventory extends PartialType(CreateInventoryDto) {
 
 }
